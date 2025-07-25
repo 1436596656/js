bf520bb032dd64cd811ea1e8d79afe04090dca29
1436596656 <115448661+1436596656@users.noreply.github.com>
2025-07-25 11:21:04 +0800

    Update userActiveLog.js

--- userActiveLog.js
+++ userActiveLog.js
@@ -1,6 +1,6 @@
 var t1 = new Date().getTime();
 // 获取浏览器你去类型和版本 手机端直接获取手机型号
-// 0724 1900-111
+// 0725 11
 function getBrowserInfo() {
   let agent = navigator.userAgent.toLowerCase();
   let regStr_ie = /msie[\d.]+/gi;
