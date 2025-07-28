bac18272131f800afb17381db6e654721618dc5a
1436596656 <115448661+1436596656@users.noreply.github.com>
2025-07-28 14:05:27 +0800

    Update userActiveLog.js

--- userActiveLog.js
+++ userActiveLog.js
@@ -1,6 +1,6 @@
 var t1 = new Date().getTime();
 // 获取浏览器你去类型和版本 手机端直接获取手机型号
-// 0728 10:06
+// 0728 14:05
 function getBrowserInfo() {
   let agent = navigator.userAgent.toLowerCase();
   let regStr_ie = /msie[\d.]+/gi;
